9b98a90690ab8e6de04f916759a81304f5765de7
SurendraKothuruKumar <103406764+SurendraKothuruKumar@users.noreply.github.com>
2022-04-11 12:18:52 +0530

    Add files via upload

---
+++ EmployeeProjectManagement/src/test/java/com/training/EmployeeProjectManagement/EmployeeProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.EmployeeProjectManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
